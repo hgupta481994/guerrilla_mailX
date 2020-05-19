fb9c64c0cd7a6abf12fc54b87da8cdc9f623204e
himanshu-thinkfuture <gupta.himanshu@thinkfuture.us>
2020-05-19 21:07:11 +0530

    0.0.1 done

--- lib/guerrillamailchecker/api.rb
+++ lib/guerrillamailchecker/api.rb
@@ -11,55 +11,93 @@ require 'pry'
 # require 'clipboard'
 # require 'rspec'
 
-module Guerrilla
-	def self.get_email_address(email_id = nil)
-		response = send_request("POST", "https://api.guerrillamail.com/ajax.php?f=get_email_address")
-	  sid_token = response[:body][:sid_token]
-	
+class Guerrilla
+  @sid_token
+  @email
+  @time
+  @timestamp
+  @alias_id
+  @valid_till
+
+
+  attr_accessor :sid_token, :email, :time, :timestamp, :alias_id, :valid_till
+
+  def initialize(email_id = nil, sid_token: nil)
+    if !sid_token.present?
+      response = send_request("POST", "https://api.guerrillamail.com/ajax.php?f=get_email_address")
+      @sid_token = sid_token = response[:body][:sid_token]
+    else
+      @sid_token = sid_token
+    end
+  
     if email_id.present?
-			set_email_url = "https://api.guerrillamail.com/ajax.php?f=set_email_user&email_user=#{email_id}&lang=en&sid_token=#{sid_token}"
-			response = send_request("POST",set_email_url)
-			
-	    	# site_id = response[:body][:site_id]
-	    	# site = response[:body][:site]
-	  end
-	  email_got = response[:body][:email_addr]
-		alias_id  = response[:body][:alias]
-		timestamp = response[:body][:email_timestamp]
-  	time      = Time.at(timestamp).to_datetime.to_s
-    valid_till = Time.at(timestamp+3600).to_datetime.to_s
-    return {email: email_got, sid_token: sid_token, time: time, alias_id: alias_id, valid_till: valid_till}
-	end
+      set_email_url = "https://api.guerrillamail.com/ajax.php?f=set_email_user&email_user=#{email_id}&lang=en&sid_token=#{sid_token}"
+      response = send_request("POST",set_email_url)
+        # site_id = response[:body][:site_id]
+        # site = response[:body][:site]
+    end
+    @email = response[:body][:email_addr]
+    @alias_id  = response[:body][:alias]
+    @timestamp = response[:body][:email_timestamp]
+    @time      = Time.at(timestamp).to_datetime.to_s
+    @valid_till = Time.at(timestamp+3600).to_datetime.to_s
+  end
 
-  def self.verify_last_sent_mail sid_token: nil, wait: 120, body: nil, subject: nil
+	def self.get_email_address
+		response = send_request("POST", "https://api.guerrillamail.com/ajax.php?f=get_email_address")
+  end
 
+  def set_email_user(sid_token: nil, email_user: nil, lang: 'en', site: nil)
+    if !sid_token.present?
+      sid_token = @sid_token
+    end
+    set_email_url = "https://api.guerrillamail.com/ajax.php?f=set_email_user&email_user=#{email_user}&lang=#{lang}&sid_token=#{sid_token}&site=#{site}"
+    response = send_request("POST",set_email_url)
+  end
+
+  def verify_last_sent_mail sid_token: nil, wait: 120, body: nil, subject: nil
+    if !subject.present?
+      return "ATTRIBUTE REQUIRED: No subject given"
+    end
+
+    if !sid_token.present?
+      sid_token = @sid_token
+    end
     get_email_list_url = "https://api.guerrillamail.com/ajax.php?f=get_email_list&offset=0&sid_token=#{sid_token}"
-    result =[]
+    result = {}
     start_minute = Time.now
     while Time.now <= start_minute + wait
       sleep(5)
       response = send_request("POST",get_email_list_url)
+      return "No Mail Present" if response[:body][:list][0] == nil
       mail_id = response[:body][:list][0][:mail_id]
       fetch_email_url = "https://api.guerrillamail.com/ajax.php?f=fetch_email&email_id=#{mail_id}&sid_token=#{sid_token}"
       email_response = send_request("POST",fetch_email_url)
       break if (email_response[:body][:mail_subject]).include?(subject.to_s) 
     end
     if email_response[:body][:mail_subject].include?(subject)
-    	result << "subject present"
+    	result[:subject] = true
     else
-    	result << "subject not present"
+      result[:subject] = false
     end
     if body.present?
     	if email_response[:body][:mail_body].include?(body)
-	    	result << "body present"
+        result[:body] = true
 	    else
-	    	result << "body not present"
+        result[:body] = false
 	    end
     end
     return result
   end
 
-  def self.verify_mail sid_token: nil, email_id: nil, offset: nil, wait: 120, body: nil, subject: nil
+  def verify_mail sid_token: nil, email_id: nil, offset: nil, wait: 120, body: nil, subject: nil
+    if !subject.present?
+      return "ATTRIBUTE REQUIRED: No subject given"
+    end
+    
+    if !sid_token.present?
+      sid_token = @sid_token
+    end
 
     if !email_id.present?
       if offset.present?
@@ -69,12 +107,13 @@ module Guerrilla
       end
     end
 
-    result =[]
+    result ={}
     start_minute = Time.now
     while Time.now <= start_minute + wait
       sleep(5)
       if !email_id.present?
         response = send_request("POST",get_email_list_url)
+        return "No Mail Present" if response[:body][:list][0] == nil
         mail_id = response[:body][:list][0][:mail_id]
       else
         mail_id = email_id
@@ -84,59 +123,81 @@ module Guerrilla
       break if (email_response[:body][:mail_subject]).include?(subject.to_s) 
     end
     if email_response[:body][:mail_subject].include?(subject)
-      result << "subject present"
+      result[:subject] = true
     else
-      result << "subject not present"
+      result[:subject] = false
     end
     if body.present?
       if email_response[:body][:mail_body].include?(body)
-        result << "body present"
+        result[:body] = true
       else
-        result << "body not present"
+        result[:body] = false
       end
     end
     return result
   end
 
-  def self.check_email(sid_token, seq = 0)
+  def check_email(seq: 0, sid_token: nil)
+    if !sid_token.present?
+      sid_token = @sid_token
+    end
   	check_email_url = "https://api.guerrillamail.com/ajax.php?f=check_email&seq=#{seq}&sid_token=#{sid_token}"
   	response = send_request("POST",check_email_url)
   	return response
   end
 
-  def self.get_email_list(sid_token: nil, offset: 0, seq: 0)
+  def get_email_list(sid_token: nil, offset: 0, seq: 0)
+    if !sid_token.present?
+      sid_token = @sid_token
+    end
   	get_email_list_url = "https://api.guerrillamail.com/ajax.php?f=get_email_list&offset=#{offset}&seq=#{seq}&sid_token=#{sid_token}"
   	response = send_request("POST",get_email_list_url)
   	return response
   end
 
-  def self.fetch_email(sid_token: nil, email_id: nil)
-    fetch_email_url = "https://api.guerrillamail.com/ajax.php?f=fetch_email&sid_token=#{sid_token}&email_id=#{email_id}"
+  def fetch_email(sid_token: nil, mail_id: nil)
+    if !mail_id.present?
+      return "ATTRIBUTE REQUIRED: No mail_id given"
+    end
+    if !sid_token.present?
+      sid_token = @sid_token
+    end
+    fetch_email_url = "https://api.guerrillamail.com/ajax.php?f=fetch_email&sid_token=#{sid_token}&email_id=#{mail_id}"
     response = send_request("POST",fetch_email_url)
   end
 
-	def self.forget_me(sid_token: nil, email_addr: nil)
+	def forget_me(sid_token: nil, email_addr: nil)
+    if !sid_token.present?
+      sid_token = @sid_token
+    end
 		forget_sid_url = "https://api.guerrillamail.com/ajax.php?f=forget_me&sid_token=#{sid_token}&email_addr=#{email_addr}"
 		response = send_request("POST",forget_sid_url)
 	end
 
-  def self.del_email(sid_token: nil, email_ids: nil)
+  def del_email(sid_token: nil, email_ids: nil)
+    if !sid_token.present?
+      sid_token = @sid_token
+    end
     email_s = ""
     if email_ids.present?
       email_ids.each do |e|
-      email_s += "&email_ids[]=#{e}"  
+        email_s += "&email_ids[]=#{e}"  
+      end
     end
     del_email_url = "https://api.guerrillamail.com/ajax.php?f=del_email&sid_token=#{sid_token}#{email_s}"
     response = send_request("POST",del_email_url)
   end
 
-  def self.get_older_list(sid_token: nil, seq: nil, limit: nil)
+  def get_older_list(sid_token: nil, seq: nil, limit: nil)
+    if !sid_token.present?
+      sid_token = @sid_token
+    end
     get_older_list_url = "https://api.guerrillamail.com/ajax.php?f=get_older_list&sid_token=#{sid_token}&seq=#{seq}&limit=#{limit}"
     response = send_request("POST",get_older_list_url)
   end
 
 	private
-  	def self.send_request(method, url, params={})
+  	def send_request(method, url, params={})
 	    response = RestClient::Request.execute(method: method, url: url, payload: params)
 	    if response.present? && response.code == 200
 	      {code: response.code, body: response.body.blank? ? '' : JSON.parse(response.body, symbolize_names: true) }
